79bff0f00a48f0d8370a4965f02e2112c21860a9
dasha6661 <kisame.666@mail.ru>
2025-11-10 01:02:07 +0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,111 @@
-(function initTheme() {
-  const theme = localStorage.getItem('theme');
-  if (theme) {
-    setTheme(theme);
-  }
-})();
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const plumber = require('gulp-plumber');
+const del = require('del');
+const concat = require('gulp-concat-css');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const media_query = require('postcss-combine-media-query');
+const cssnano = require('cssnano');
+const htmlMinify = require('html-minifier');
+const jsMinify = require('gulp-minify');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const currentTheme = [...document.documentElement.classList]
-    .find((cn) => cn.startsWith('theme-'))
-    ?.replace('theme-', '');
-  const themeButtons = [
-    ...document.querySelectorAll('.header__theme-menu-button'),
+function clean() {
+  return del('dist');
+}
+
+function html() {
+  const options = {
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+    collapseWhitespace: true,
+    minifyCSS: true,
+    keepClosingSlash: true
+  };
+  return gulp.src('src/**/*.html')
+    .pipe(plumber())
+    .on('data', function (file) {
+      const bufferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
+      return file.contents = bufferFile
+    })
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function css() {
+  const plugins = [
+    autoprefixer(),
+    media_query(),
+    cssnano()
   ];
-  setActiveButton(themeButtons, currentTheme);
-
-  themeButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const chosenTheme = [...button.classList]
-        .find((cn) => cn.includes('_type_'))
-        .split('_type_')[1];
-      setTheme(chosenTheme);
-      setActiveButton(themeButtons, chosenTheme);
-    });
-  });
-});
-
-function setTheme(theme) {
-  document.documentElement.className = '';
-  document.documentElement.classList.add(`theme-${theme}`);
-  localStorage.setItem('theme', theme);
+  return gulp.src('src/**/*.css')
+    .pipe(plumber())
+    .pipe(concat('bundle.css'))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
-function setActiveButton(buttonsArray, theme) {
-  buttonsArray.forEach((button) => {
-    button.classList.remove('header__theme-menu-button_active');
-    button.removeAttribute('disabled');
-  });
-  const target = buttonsArray.find((button) =>
-    button.classList.contains(`header__theme-menu-button_type_${theme}`)
-  );
-  if (target) {
-    target.classList.add('header__theme-menu-button_active');
-    target.setAttribute('disabled', true);
-  } else {
-    const autoButton = document.querySelector(
-      '.header__theme-menu-button_type_auto'
-    );
-    autoButton.classList.add('header__theme-menu-button_active');
-    autoButton.setAttribute('disabled', true);
-  }
+function images() {
+  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.reload({ stream: true }));
 }
+
+function fonts() {
+  return gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function videos() {
+  return gulp.src('src/videos/**/*.{avi,mp4,webm}')
+    .pipe(gulp.dest('dist/videos'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function scripts() {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(jsMinify({
+      ext: {
+        min: '.js'
+      },
+      noSource: true
+    }))
+    .pipe(gulp.dest('dist/scripts'))
+}
+
+function watchFiles() {
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/**/*.css'], css);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/fonts/**/*.{ttf,woff,woff2}'], fonts);
+  gulp.watch(['src/videos/**/*.{avi,mp4,webm}'], videos);
+  gulp.watch(['src/scripts/**/*.{js}'], scripts);
+}
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts, videos, scripts));
+const watchapp = gulp.parallel(build, watchFiles, serve);
+
+exports.html = html;
+exports.css = css;
+exports.images = images;
+exports.fonts = fonts;
+exports.videos = videos;
+exports.scripts = scripts;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watchapp;
+exports.default = watchapp;
